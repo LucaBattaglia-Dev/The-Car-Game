94e5e7f18ec28c64da635188b88eb24b10f79004
Luca <253631961+LucaBattaglia-Dev@users.noreply.github.com>
2026-01-27 16:07:04 +1100

    Squashed commit of the following:
    commit e8b8172d0c5179d3f3b72a56634531bdd9124e11
    Merge: 57cd722 c66f233
    Author: Luca <253631961+LucaBattaglia-Dev@users.noreply.github.com>
    Date:   Tue Jan 27 16:06:11 2026 +1100
    
        Merge branch 'Movement-Branch' into Test-Branch
    
    commit c66f2330045880f07038a5bce6fb9775182d7f74
    Author: Luca <253631961+LucaBattaglia-Dev@users.noreply.github.com>
    Date:   Tue Jan 27 16:05:40 2026 +1100
    
        Turning Bug Fix
    
    commit 6935693ce078a7ffac7c3d557ac3ba2c78a373f5
    Author: Luca <253631961+LucaBattaglia-Dev@users.noreply.github.com>
    Date:   Tue Jan 27 16:05:03 2026 +1100
    
        Revert "Turning Bug Fix"
    
        This reverts commit 941e5fd1ccef99c718888196782344e71f8207d7.
    
    commit 941e5fd1ccef99c718888196782344e71f8207d7
    Author: Luca <253631961+LucaBattaglia-Dev@users.noreply.github.com>
    Date:   Tue Jan 27 16:04:40 2026 +1100
    
        Turning Bug Fix
    
    commit c2f5057f256c3d7935ddc0d7598d4ad74a9bb0a9
    Author: Luca <253631961+LucaBattaglia-Dev@users.noreply.github.com>
    Date:   Tue Jan 27 15:57:58 2026 +1100
    
        Car Movement Update
    
    commit 70f9b6221e507e0dec9d8b22db8ea94a9cd35cf9
    Author: Luca <253631961+LucaBattaglia-Dev@users.noreply.github.com>
    Date:   Tue Jan 27 15:39:59 2026 +1100
    
        Added Steering & Turn Curve
    
    commit 76ddf20b1393e733bbbcba068e6962a389ef7227
    Author: Luca <253631961+LucaBattaglia-Dev@users.noreply.github.com>
    Date:   Tue Jan 27 14:54:21 2026 +1100
    
        Back & Forth Movement
    
    commit 57cd7220e38deeae6c6d48e719381a8ab18382b4
    Merge: e84655c 6042828
    Author: Luca <253631961+LucaBattaglia-Dev@users.noreply.github.com>
    Date:   Tue Jan 27 13:06:09 2026 +1100
    
        Merge branch 'Test-Branch' of https://github.com/LucaBattaglia-Dev/The-Car-Game into Test-Branch
    
    commit e84655ce9024a17b6a9b3355c9feb2206e067ba1
    Merge: f7be19a cd52309
    Author: Luca <253631961+LucaBattaglia-Dev@users.noreply.github.com>
    Date:   Tue Jan 27 13:06:00 2026 +1100
    
        Merge branch 'Movement-Branch' into Test-Branch
    
    commit 604282869f75502cc32df9659244969b7cc50d1a
    Author: HarleyDoolan <136434383+HarleyJamesD@users.noreply.github.com>
    Date:   Tue Jan 27 13:05:33 2026 +1100
    
        Second update
    
    commit cd5230962d03e7ad734e73ecbb23ce18eff52894
    Author: Luca <253631961+LucaBattaglia-Dev@users.noreply.github.com>
    Date:   Tue Jan 27 13:04:18 2026 +1100
    
        Adding Car models & Suspension
    
    commit f7be19a102c37cb2f8f8e57752251c35f949abb4
    Author: HarleyDoolan <136434383+HarleyJamesD@users.noreply.github.com>
    Date:   Tue Jan 27 12:59:27 2026 +1100
    
        Draft level and road

--- Assets/Scripts/CarController.cs
+++ Assets/Scripts/CarController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using UnityEngine;
 using System;
+using System.IO;
 using System.Collections.Generic;
 
 public class CarController : MonoBehaviour
@@ -9,6 +10,7 @@ public class CarController : MonoBehaviour
     [SerializeField] private Rigidbody carRB;
     [SerializeField] private Transform[] rayPoints;
     [SerializeField] private LayerMask drivable;
+    [SerializeField] private Transform accelerationPoint;
 
     [Header("Suspension Settings")]
     [SerializeField] private float springStiffness;
@@ -17,6 +19,27 @@ public class CarController : MonoBehaviour
     [SerializeField] private float springTravel;
     [SerializeField] private float wheelRadius;
 
+    private int[] wheelsIsGrounded = new int[4];
+    private bool isGrounded = false;
+
+    [Header("Input")]
+    private float moveInput = 0;
+    private float steerInput = 0;
+
+    [Header("Car Settings")]
+    [SerializeField] private float acceleration = 25f;
+    [SerializeField] private float maxSpeed = 100f;
+    [SerializeField] private float deceleration = 10f;
+    [SerializeField] private float steerStrength = 15f;
+    [SerializeField] private AnimationCurve turningCurve;
+    [SerializeField] private float dragCoefficient = 1f;
+
+    private Vector3 currentCarLocalVelocity = Vector3.zero;
+    private float carVelocityRatio = 0;
+
+    //==============================================================================
+    //Unity UpdateSystems Below
+    //==============================================================================
 
     public void Start()
     {
@@ -26,40 +49,139 @@ public class CarController : MonoBehaviour
     public void FixedUpdate()
     {
         Suspension();
+        GroundCheck();
+        CalculateCarVelocity();
+        Movement();
+    }
+
+    private void Update()
+    {
+        GetPlayerInput();
+    }
+
+    //==============================================================================
+    //Unity Functions Below
+    //==============================================================================
+
+    private void Movement()
+    {
+        if (isGrounded)
+        {
+            Acceleration();
+            Decelration(); // Note: Spelling matches image provided
+            Turn();
+            SidewaysDrag();
+        }
+    }
+
+    private void Acceleration()
+    {
+        // Applies forward force at the designated acceleration point
+        carRB.AddForceAtPosition(acceleration * moveInput * transform.forward, accelerationPoint.position, ForceMode.Acceleration);
+    }
+
+    private void Decelration()
+    {
+        // Applies counter-force based on input
+        carRB.AddForceAtPosition(deceleration * moveInput * -transform.forward, accelerationPoint.position, ForceMode.Acceleration);
+    }
+
+    private void Turn()
+    {
+
+        carRB.AddTorque(steerStrength * steerInput * turningCurve.Evaluate(carVelocityRatio) * Mathf.Sign(carVelocityRatio) * transform.up, ForceMode.Acceleration);
     }
     
+    private void SidewaysDrag()
+    {
+        float currentSidewaysSpeed = currentCarLocalVelocity.x;
+        float dragMagnitude = -currentSidewaysSpeed * dragCoefficient;
+        Vector3 dragForce = transform.right * dragMagnitude;
+
+        // Applies counter-force to reduce sideways sliding
+        carRB.AddForceAtPosition(dragForce, carRB.worldCenterOfMass, ForceMode.Acceleration);
+    }
+
+
+    private void GetPlayerInput()
+    {
+        // Captures W/S or Up/Down arrows (-1 to 1)
+        moveInput = Input.GetAxis("Vertical");
+
+        // Captures A/D or Left/Right arrows (-1 to 1)
+        steerInput = Input.GetAxis("Horizontal");
+    }
+
     private void Suspension()
     {
-        foreach (Transform rayPoint in rayPoints)
+        for (int i = 0; i < rayPoints.Length; i++)
         {
             RaycastHit hit;
             float maxLength = restLength + springTravel;
 
-            if (Physics.Raycast(rayPoint.position, -rayPoint.up, out hit, maxLength + wheelRadius, drivable))
+            // Cast a ray downwards from each suspension point
+            if (Physics.Raycast(rayPoints[i].position, -rayPoints[i].up, out hit, maxLength + wheelRadius, drivable))
             {
-                // Calculate distance and compression
+                wheelsIsGrounded[i] = 1;
+
+                // Calculate how much the spring is compressed
                 float currentSpringLength = hit.distance - wheelRadius;
                 float springCompression = (restLength - currentSpringLength) / springTravel;
 
-                // Calculate damp force (velocity-based resistance)
-                float springVelocity = Vector3.Dot(carRB.GetPointVelocity(rayPoint.position), rayPoint.up);
+                // Calculate the velocity of the suspension point to determine damping
+                float springVelocity = Vector3.Dot(carRB.GetPointVelocity(rayPoints[i].position), rayPoints[i].up);
                 float dampForce = damperStiffness * springVelocity;
 
-                // Calculate spring force (position-based resistance)
+                // Hooke's Law: Force = Stiffness * Compression
                 float springForce = springStiffness * springCompression;
 
-                // Calculate net force and apply it
+                // Combine forces (Spring pulls up, Damper resists movement)
                 float netForce = springForce - dampForce;
-                carRB.AddForceAtPosition(netForce * rayPoint.up, rayPoint.position);
 
-                // Debug visualization
-                Debug.DrawLine(rayPoint.position, hit.point, Color.red);
+                // Apply the force to the Rigidbody at the specific wheel position
+                carRB.AddForceAtPosition(netForce * rayPoints[i].up, rayPoints[i].position);
+
+                // Visual debugging: Red line when grounded
+                Debug.DrawLine(rayPoints[i].position, hit.point, Color.red);
             }
             else
             {
-                // Visualization when the wheel is off the ground
-                Debug.DrawLine(rayPoint.position, rayPoint.position + (wheelRadius + maxLength) * -rayPoint.up, Color.green);
+                wheelsIsGrounded[i] = 0;
+
+                // Visual debugging: Line showing max reach when in the air
+                Debug.DrawLine(rayPoints[i].position, rayPoints[i].position + (wheelRadius + maxLength) * -rayPoints[i].up, Color.white);
             }
         }
     }
+
+    private void GroundCheck()
+    {
+        int tempGroundedWheels = 0;
+
+        // Loop through each wheel to count how many are currently touching the ground
+        for (int i = 0; i < wheelsIsGrounded.Length; i++)
+        {
+            tempGroundedWheels += wheelsIsGrounded[i];
+        }
+
+        // If more than one wheel is grounded, the vehicle is considered grounded
+        if (tempGroundedWheels > 1)
+        {
+            isGrounded = true;
+        }
+        else
+        {
+            isGrounded = false;
+        }
+    }
+
+    private void CalculateCarVelocity()
+    {
+        // Converts world-space velocity into the car's local forward/right/up directions
+        currentCarLocalVelocity = transform.InverseTransformDirection(carRB.linearVelocity);
+        
+        // Calculates a 0 to 1 (or higher) ratio of current speed vs max speed
+        carVelocityRatio = currentCarLocalVelocity.z / maxSpeed;
+    }
+
 }
